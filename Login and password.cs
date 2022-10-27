8be595f7c671800f4d0d87491a5eb6e8bf725133
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2022-10-26 20:05:30 -0400

    Add files via upload

---
+++ Login and password.cs
@@ -0,0 +1,27 @@
+using System;
+class Program {
+  public static void Main (string[] args) {
+    
+    Console.WriteLine("Enter your imformation");
+    Console.WriteLine("User name:");
+    string username=Console.ReadLine();
+    Console.WriteLine("Password:");
+    string password= Console.ReadLine();
+    if(username=="Grade10" && password=="12345"){
+      Console.WriteLine("login successfully");
+    }
+    else{
+      Console.WriteLine("fail to login");
+    }
+    
+   
+    
+
+
+    
+    
+  
+    
+  
+  }
+}
