19b1168654e6ff473e18eca29f0f687275885a14
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2022-11-03 14:04:12 -0400

    Add files via upload

---
+++ multiple-choice questions.cs
@@ -0,0 +1,76 @@
+//difference between while and do while maen
+using System;
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine("There are 5 multiple-choice questions related to programming concepts below, please write down the correct answer");
+    Console.WriteLine("1. Whatoperator can use as AND ");
+    Console.WriteLine("a. !()");
+    Console.WriteLine("b. &&");
+    Console.WriteLine("c. *");
+    Console.WriteLine("d. ||");
+    Console.WriteLine("Answer:");
+    string answer1=Console.ReadLine();
+  if(answer1=="b"){
+    Console.WriteLine("Correct Answer");
+  }
+  else{
+    Console.WriteLine("Incorrect Answer");
+  }
+    
+    Console.WriteLine("2. Which one is correct if a=5, b=a+1 ");
+    Console.WriteLine("a. a+b=12");
+    Console.WriteLine("b. a++ + b=13");
+    Console.WriteLine("c. a*b=5");
+    Console.WriteLine("d. ++a + b=13");
+    Console.WriteLine("Answer:");
+    string answer2=Console.ReadLine();
+  if(answer2=="d"){
+    Console.WriteLine("Correct Answer");
+  }
+  else{
+    Console.WriteLine("Incorrect Answer");
+      }
+    
+    Console.WriteLine("3. What does '!' usually means");
+    Console.WriteLine("a. It is really important");
+    Console.WriteLine("b. This is an error");
+    Console.WriteLine("c. To reverses the logical state of its operand ");
+    Console.WriteLine("d. Amazing things");
+    Console.WriteLine("Answer:");
+    string answer3=Console.ReadLine();
+  if(answer3=="c"){
+    Console.WriteLine("Correct Answer");
+  }
+  else{
+    Console.WriteLine("Incorrect Answer");
+      }
+    
+    Console.WriteLine("What conditional statement can write after 'if' ");
+    Console.WriteLine("a. for");
+    Console.WriteLine("b. do");
+    Console.WriteLine("c. else ");
+    Console.WriteLine("d. whlie");
+    Console.WriteLine("Answer:");
+    string answer4=Console.ReadLine();
+  if(answer4=="c"){
+    Console.WriteLine("Correct Answer");
+  }
+  else{
+    Console.WriteLine("Incorrect Answer");
+      }
+    
+    Console.WriteLine("Which keyword is used to decimal numbers for example 3.5 ");
+    Console.WriteLine("a. int");
+    Console.WriteLine("b. doule");
+    Console.WriteLine("c. char ");
+    Console.WriteLine("d. string");
+    Console.WriteLine("Answer:");
+    string answer5=Console.ReadLine();
+  if(answer5=="b"){
+    Console.WriteLine("Correct Answer");
+  }
+  else{
+    Console.WriteLine("Incorrect Answer");
+      }
+  }
+}
